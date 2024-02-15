90928ff97571faedc6ec44b7de5a36f8d54e1428
IQUELFergal <fergal.iquel@gmail.com>
2024-02-15 13:46:17 -0500

    Add age randomness

--- Assets/Scripts/CharacterGenerator/Character.cs
+++ Assets/Scripts/CharacterGenerator/Character.cs
@@ -1,8 +1,18 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Character
 {
-    static float hasIDProbability = 0.85f; 
+    static DateTime today = new DateTime(2826, 02, 04);
+    static int majorityAge = 18;
+    static int maxAgeDifferenceAboveMajority = 25;
+    static int minAgeDifferenceBelowMajority = 2;
+    static int dayInAYear = 365;
+
+
+    static float hasIDProbability = 0.85f;
+    static float isIDValidProbability = 0.85f;
 
     public string firstName => m_firstName;
     private string m_firstName;
@@ -19,25 +29,37 @@ public class Character
     public CharacterEffect characterEffect => m_characterEffect;
     private CharacterEffect m_characterEffect;
 
-    public DateTime dateOfBirth => m_dateOfBirth; //Generer une date en fonction de si l'id est valide ou non, un range (15-17) si invalide, un range (18-
+    public DateTime dateOfBirth => m_dateOfBirth;
     private DateTime m_dateOfBirth;
 
     public bool hasID => m_hasID;
     private bool m_hasID;
-    public bool isIDValid => false; // 28eme siecle, majorite a 18 ans
+    public bool isIDValid => m_isIDValid;
+    private bool m_isIDValid;
+
+    public int age => (today - m_dateOfBirth).Days / 365;
+
     public bool isDrunk => m_characterEffect.HasFlag(CharacterEffect.Drunk);
     public bool isHigh => m_characterEffect.HasFlag(CharacterEffect.High);
-
 
     public Character(CharacterSpecie characterSpecie, CharacterTrait characterTrait)
     {
         m_firstName = characterSpecie.GetRandomCharacterFirstName();
         m_lastName = characterSpecie.GetRandomCharacterLastName();
-        m_dateOfBirth = new DateTime(2800, UnityEngine.Random.Range(1,13), UnityEngine.Random.Range(1, 30));
-        m_hasID = UnityEngine.Random.Range(0f, 1f) < 0.85f; 
+
         m_characterSpecie = characterSpecie;
         m_characterTrait = characterTrait;
         m_characterEffect = characterTrait.GetRandomEffect();
+
+        m_hasID = UnityEngine.Random.Range(0f, 1f) < hasIDProbability;
+        m_isIDValid = UnityEngine.Random.Range(0f, 1f) < hasIDProbability * isIDValidProbability;
+        // isIDValidAge et isIDValidLogo
+
+
+        m_dateOfBirth = today - (m_isIDValid ? new TimeSpan((UnityEngine.Random.Range(majorityAge, majorityAge + maxAgeDifferenceAboveMajority + 1) * dayInAYear + UnityEngine.Random.Range(0, dayInAYear + 1)), 0, 0, 0) :
+        //                                                   random year number between majorityAge and majorityAge + maxAgeDifferenceAboveMajority * dayInAYear - random day offset   
+                                               new TimeSpan((UnityEngine.Random.Range(majorityAge - minAgeDifferenceBelowMajority, majorityAge + 1) * dayInAYear) - UnityEngine.Random.Range(1, dayInAYear + 1), 0, 0, 0));
+        //                                                   random year number between majorityAge - minAgeDifferenceBelowMajority and majorityAge * dayInAYear + random day offset (added so it becomes less than majority                                                              
     }
     public void SetName(string firstName, string lastName)
     {
