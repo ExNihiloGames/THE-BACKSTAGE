151a97e00f24a73bc68f8ec6c7dd7347849480df
williamedery <william.edery@hotmail.fr>
2024-02-18 01:50:06 -0500

    Commit_RestoreChanges

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,85 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BAR_AMBIANCE = 151359411U;
+        static const AkUniqueID CARD_PLAYER = 2984839113U;
+        static const AkUniqueID CARD_REFUSED = 3086388350U;
+        static const AkUniqueID CARD_VALID = 3887501564U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAY_GROGNEMENTS = 488222583U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_LEVEL1
+        {
+            static const AkUniqueID GROUP = 2209143526U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SOUNDCHECK = 1056444866U;
+                static const AkUniqueID TUNE1 = 932294504U;
+                static const AkUniqueID TUNE2 = 932294507U;
+                static const AkUniqueID TUNE3 = 932294506U;
+            } // namespace STATE
+        } // namespace MUSIC_LEVEL1
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace PEOPLE_NUMBER
+        {
+            static const AkUniqueID GROUP = 2217972468U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LEVEL1 = 2678230382U;
+                static const AkUniqueID LEVEL2 = 2678230381U;
+                static const AkUniqueID LEVEL3 = 2678230380U;
+            } // namespace SWITCH
+        } // namespace PEOPLE_NUMBER
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CROWD_EXCITMENT = 2786753164U;
+        static const AkUniqueID PEOPLE_NUMBER = 2217972468U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID THEBACKSTAGE_MUSIC = 743795859U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MENU_BUS = 3022218555U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
